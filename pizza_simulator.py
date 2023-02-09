66e4bbc8891ac7bdfa845e4eaaf7739a2d665872
porat-ah <porat2001@gmail.com>
2023-02-09 18:24:56 +0200

    implement simulation manager

--- pizza_simulator.py
+++ pizza_simulator.py
@@ -1,9 +1,11 @@
 import numpy as np
 import json
 import pandas as pd
-import time
+import datetime
 
 TOPPINGS = ['olive', 'mushrooms', 'corn', 'onion', 'tuna', 'jalapeno']
+
+
 class order_id:
     num = 0
 
@@ -16,6 +18,7 @@ class order_id:
         out = self.num
         self.num += 1
         return out
+
 
 class branch:
     def __init__(self, loc, branch_id, name, average_order_time, std_order_time, toppings_dist, num_of_topping_dist) -> None:
@@ -55,6 +58,8 @@ class branch:
         return self.open
 
 
+
+
 class order:
     def __init__(self, id, num_of_toppings,toppings, time, order_time_length, branch ) -> None:
         self.id = id
@@ -63,6 +68,79 @@ class order:
         self.time = time
         self.order_time_length = order_time_length
         self.branch = branch
+        self.sent_count = 0
     
     def order_to_json(self):
-        pass
+        order_status = "in_process" if self.is_order_in_process() else "finished"
+        if self.sent_count == 1 and order_status == "in_process":
+            return
+        _order = pd.DataFrame({
+            "id": self.id,
+            "branch_id" : self.branch.branch_id,
+            "barnch_name": self.branch.name,
+            "location": self.branch.loc,
+            "time": str(self.time),
+            "order_status": order_status,
+            "number_of_toppings": self.num_of_toppings,
+            "toppings": self.toppings
+        })
+        self.sent_count += 1
+        return _order.to_json()
+    
+    def is_order_in_process(self):
+        return self.time + datetime.timedelta(minutes= self.order_time_length) <  datetime.datetime.now()
+
+
+
+
+class simulation_manager:
+    def __init__(self) -> None:
+        self.branches = []
+        self.orders = []
+    
+    def create_branch(self, opening_time, closing_time, branch, order_prob):
+        _branch_meta_data = branch_meta_data(branch, opening_time, closing_time, order_prob)
+        self.branches.append(_branch_meta_data)
+
+
+    def run_simulation(self,time): 
+        for branch in self.branches:
+            self.close_open(branch, time)
+            if branch.branch.is_open():
+                num_of_orders = np.random.choice([0, 1 ,2 ,3], size= 1, p= [.6, .25, .1,.05])
+                orders_to_create = self.num_of_orders_to_create(branch, num_of_orders)
+                for b in orders_to_create:
+                    if b:
+                        self.orders.append(branch.branch.create_order(time))
+        orders_to_delete =[]
+        for order in self.orders:
+            if order.sent_count == 2:
+                orders_to_delete.append(order)
+            else:
+                order.order_to_json()
+        for order in orders_to_delete:
+            self.orders.remove(order)
+
+
+    def close_open(self, branch, time):
+        closing = time.replace(hour=branch.opening_time.hour, minute=branch.opening_time.minute, second=0, microsecond=0)
+        opening = time.replace(hour=branch.opening_time.hour, minute=branch.opening_time.minute, second=0, microsecond=0)
+        if time > closing:
+            branch.branch.close()
+        elif time > opening:
+            branch.branch.open()
+
+
+    def num_of_orders_to_create(self, branch, num):
+        return np.random.choice([True, False], size= num, p= [branch.order_prob, 1- branch.order_prob])
+
+
+
+
+
+class branch_meta_data:
+    def __init__(self, branch, opening_time, closing_time, order_prob) -> None:
+        self.branch = branch
+        self.opening_time = opening_time
+        self.closing_time = closing_time
+        self.order_prob = order_prob
